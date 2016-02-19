8dd93cbb3d515e3eb9127a2800a8ffe32426c9a3
Emi1305 <edelpeon@gmail.com>
2016-02-19 17:50:38 -0300

    Uso de templates, se permite votar y se muestran los resultados

--- django-test/probando/polls/urls.py
+++ django-test/probando/polls/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'), #/polls/
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'), #/polls/01/
+    url(r'^(?P<question_id>\d+)/result/$', views.results, name='results'), #/polls/01/result/
+    url(r'^(?P<question_id>\d+)/votes/$', views.vote, name='votes'), #/polls/01/votes/
 ]
--- django-test/probando/polls/views.py
+++ django-test/probando/polls/views.py
@@ -1,9 +1,41 @@
+from urllib import request
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello worls. This is the polls index')
+    from .models import Question
+    #template = loader.get_template('polls/index.html')
+    preguntas = Question.objects.order_by('-pub_date')
+    #response = '<br>'.join([p.question_text for p in preguntas])
+    #return HttpResponse('Hello world. This is the polls index<br><br>' + response)
+    context = {'preguntas': preguntas}
+    #return HttpResponse(template.render(context=context, request=request))
+    return render(request,'polls/index.html',context)
 
 def detail(request, question_id):
-    return HttpResponse
+    from .models import Question
+    pregunta = get_object_or_404(Question, pk=question_id)
+    context = {'pregunta': pregunta}
+    return render(request, 'polls/detail.html', context)
+
+def vote(request, question_id):
+    from .models import Question, Choice
+    pregunta = get_object_or_404(Question, pk=question_id)
+    try:
+        choice = Choice.objects.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'pregunta':pregunta, 'error_message':'No selecciono ningun opcion.'})
+    else:
+        choice.votes += 1
+        choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+def results(request, question_id):
+    from .models import Question, Choice
+    pregunta = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'pregunta': pregunta})
